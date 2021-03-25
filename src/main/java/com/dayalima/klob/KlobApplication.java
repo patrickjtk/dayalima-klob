ff4c07523120448a95bc3192ff8e131cd984f597
irwan.nugraha@batmandiri.com <irwan.nugraha@batmandiri.com>
2021-03-25 20:57:39 +0700

    init

---
+++ src/main/java/com/dayalima/klob/KlobApplication.java
@@ -0,0 +1,13 @@
+package com.dayalima.klob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dayalima/klob/KlobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayalima.klob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
